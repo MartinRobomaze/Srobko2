f01a0bfb7fbb500382044ca1019be0b20825e792
Martin Robomaze <martin.robomaze@yandex.com>
2021-06-06 13:57:23 +0200

    Code examples (#11)

--- src/robot/button.h
+++ src/robot/button.h
@@ -3,8 +3,6 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 
-typedef void (*buttonPressedCallback)();
-
 class Button {
     public:
         /**
@@ -13,22 +11,15 @@ class Button {
          * @param _buttonIndex
          */
         Button(uint8_t _buttonIndex);
-
-        /**
-         * @brief Set the callback function to be called after button press.
-         * 
-         */
-        void setCallback(buttonPressedCallback);
         
         /**
-         * @brief Updates the button state.
+         * @brief Reads the current state of button.
          * 
          */
-        void update();
+        bool isPressed();
     private:
         uint8_t buttonIndex;
         uint8_t buttonPins[6] = {35, 36, 37, 38, 39, 40};
-        buttonPressedCallback callback;
 
         bool previousState = 0;
 };
